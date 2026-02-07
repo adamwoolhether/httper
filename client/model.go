faef04acebe65f57ec539fa8a81ac5afa4497757
adamwoolhether <adamwoolhether@gmail.com>
2026-02-07 23:22:21 +0800

    mv error to model

--- client/model.go
+++ client/model.go
@@ -1,6 +1,10 @@
 package client
 
-import "net/http"
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
 
 // maxErrBodySize caps the amount of response body read when
 // building an error for an unexpected status code. This prevents
@@ -10,3 +14,21 @@ const maxErrBodySize = 4 << 10 // 4KB
 
 // execFn represents a func to operate on a response.
 type execFn func(response *http.Response) error
+
+var (
+	ErrUnexpectedStatusCode = errors.New("unexpected status code")
+)
+
+type UnexpectedStatusError struct {
+	StatusCode int
+	Body       string
+	Err        error
+}
+
+func (e *UnexpectedStatusError) Error() string {
+	return fmt.Sprintf("%v: %d, body: %s", e.Err, e.StatusCode, e.Body)
+}
+
+func (e *UnexpectedStatusError) Unwrap() error {
+	return e.Err
+}
